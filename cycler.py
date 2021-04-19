76a71929e458a8309a93377d29f88dc51fb79838
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2021-04-19 00:53:04 -0600

    Update cycler.py

--- cycler.py
+++ cycler.py
@@ -4,6 +4,14 @@ import time
 import sys
 import os
 
+from datetime import datetime
+
+def timeslice():
+    '''Make a global TIMESTAMP with datetime module.'''
+    global TIMESTAMP
+    TIMESTAMP = datetime.now()
+    return(TIMESTAMP)
+
 class honeyCycle():
     '''kill and start tcpdump and remove old files in a loop forever'''
     def __init__(self, name, interval):
@@ -13,60 +21,77 @@ class honeyCycle():
 
     def tcpd(self):
         '''Kill and then start tcpdump'''
-        print ("honeyCycle >>> killing tcpdump", self.name)
+        timeslice()
+        print (TIMESTAMP, "honeyCycle >>> killing tcpdump", self.name)
         process = subprocess.Popen(('pkill', 'tcpdump'), stdout=subprocess.PIPE)
         output = process.communicate()
         process.wait()
-        print ("honeyCycle >>> starting tcpdump", self.name)
+        timeslice()
+        print (TIMESTAMP, "honeyCycle >>> starting tcpdump", self.name)
         starttcpdump = '/usr/sbin/tcpdump -nnvvXSs 9999 -i any -w /opt/net-gargoyle/$(hostname)_$(date +%Y%m%d%H%M%S)_hc.pcap &'
         os.system(starttcpdump)
         time.sleep(CYCLER.interval)
 
     def cleaner(self):
         '''Remove old files'''
-        print ("honeyCycle >>> compress fresh backup db files", self.name)
+        timeslice()
+        print (TIMESTAMP, "honeyCycle >>> compress fresh backup db files", self.name)
         gzipdbs = 'find /opt/net-gargoyle/*.db -mtime +1 -exec gzip -9 {} \;'
         os.system(gzipdbs)
-        print ("honeyCycle >>> removing old db files", self.name)
+        timeslice()
+        print (TIMESTAMP, "honeyCycle >>> removing old db files", self.name)
         olddbs = 'find /opt/net-gargoyle/*.db.gz -mtime +17 -exec rm -f {} \;'
         os.system(olddbs)
-        print ("honeyCycle >>> cleaning up old pcap files", self.name)
+        timeslice()
+        print (TIMESTAMP, "honeyCycle >>> cleaning up old pcap files", self.name)
         cleanpcap = 'find /opt/net-gargoyle/*.pcap -mtime +2 -exec rm -f {} \;'
         os.system(cleanpcap)
 
     def diskcheck(self):
         '''Check disk usage'''
-        print ("honeyCycle >>> check disk usage", self.name)
+        timeslice()
+        print (TIMESTAMP, "honeyCycle >>> check disk usage", self.name)
         sized = 'df /opt | tail -n1 | awk \'{print $5}\' | grep "^9\|^100"'
         alertsize = os.system(sized)
-        print ("honeyCycle <<< value of alertsize is", alertsize)
+        timeslice()
+        print (TIMESTAMP, "honeyCycle <<< value of alertsize is", alertsize)
         if alertsize == 0:
-            print("honeyCycle <<< Disk size problem potential, clearing pcaps.")
+            timeslice()
+            print(TIMESTAMP, "honeyCycle <<< Disk size problem potential, clearing pcaps.")
             clearpcaps = 'rm -rf /opt/net-gargoyle/*.pcap'
             os.system(clearpcaps)
         else:
-            print("honeyCycle <<< Disk usage is not concerning yet, continue on.")
-        print ("honeyCycle >>> check disk usage", self.name)
+            timeslice()
+            print(TIMESTAMP, "honeyCycle <<< Disk usage is not concerning yet, continue on.")
+        timeslice()
+        print (TIMESTAMP, "honeyCycle >>> check disk usage", self.name)
         sized2 = 'df /opt | tail -n1 | awk \'{print $5}\' | grep "^9\|^100"'
         alertsize2 = os.system(sized2)
-        print ("honeyCycle <<< value of alertsize is", alertsize)
+        timeslice()
+        print (TIMESTAMP, "honeyCycle <<< value of alertsize is", alertsize)
         if alertsize2 == 0:
-            print("honeyCycle <<< Disk size problem potential, clearing pcaps.")
+            timeslice()
+            print(TIMESTAMP, "honeyCycle <<< Disk size problem potential, clearing pcaps.")
             clearpcaps = 'rm -rf /opt/net-gargoyle/*.pcap'
             os.system(clearpcaps)
         else:
-            print("honeyCycle <<< Disk usage is not concerning yet, continue on.")
-        print ("honeyCycle >>> check disk usage", self.name)
+            timeslice()
+            print(TIMESTAMP, "honeyCycle <<< Disk usage is not concerning yet, continue on.")
+        timeslice()
+        print (TIMESTAMP, "honeyCycle >>> check disk usage", self.name)
         sized3 = 'df /opt | tail -n1 | awk \'{print $5}\' | grep "^9\|^100"'
         alertsize3 = os.system(sized3)
-        print ("honeyCycle <<< value of alertsize is", alertsize3)
+        timeslice()
+        print (TIMESTAMP, "honeyCycle <<< value of alertsize is", alertsize3)
         if alertsize3 == 0:
-            print("honeyCycle <<< Disk size problem potential, already have cleaned up our files.")
-            print("honeyCycle >>> EXIT now, manual attention to /opt needed.")
+            timeslice()
+            print(TIMESTAMP, "honeyCycle <<< Disk size problem potential, already have cleaned up our files.")
+            print(TIMESTAMP, "honeyCycle >>> EXIT now, manual attention to /opt needed.")
             sys.exit(1)
         else:
-            print("honeyCycle <<< Disk usage is not concerning yet, continue on.")          
-            
+            timeslice()
+            print(TIMESTAMP, "honeyCycle <<< Disk usage is not concerning yet, continue on.")
+
 if __name__ == '__main__':
     INTV = int(sys.argv[2])
     NAMED = str(sys.argv[1])
